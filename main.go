94f9a64d4c36d15c7c8b61dbe7f32f1227161a5c
diogo <diogomaciel.cunha@gmail.com>
2023-02-10 10:35:01 -0300

    make a route than return a json as reponse

--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 
+	"github.com/namelew/personality-api/models"
 	"github.com/namelew/personality-api/routes"
 )
 
 func main() {
+	models.Personalidades = []models.Personalidade{
+		{Nome: "Nome 1", Historia: "Historia 1"},
+		{Nome: "Nome 2", Historia: "Historia 2"},
+	}
+
 	fmt.Println("Iniciando o servidor Rest com Go")
 	routes.HandleRequest()
 }
